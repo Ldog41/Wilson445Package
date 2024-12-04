a8bf03a0f387ac2fc2e584ee6bfc659969308481
Landon Wilson <124070155+Ldog41@users.noreply.github.com>
2024-12-04 10:29:44 -0700

    Add files via upload

--- data-raw/Flagstaff_Weather.R
+++ data-raw/Flagstaff_Weather.R
@@ -5,8 +5,8 @@ library(lubridate)
 Flagstaff_Weather <- read.csv('data-raw/Pulliam_Airport_Weather_Station.csv') %>%
   select(DATE, PRCP, SNOW, TMAX,TMIN) %>%
   drop_na(.) %>%
-  filter(year(DATE) >= 2015 & year(DATE) <= 2019)
-
+  filter(year(DATE) >= 2015 & year(DATE) <= 2019) %>%
+  mutate(DATE = date(DATE))
 
 # Save the data frame to the data/ directory as Flagstaff_Weather.rda
 usethis::use_data(Flagstaff_Weather, overwrite = TRUE)
